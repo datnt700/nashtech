4fe94e8815cd87327e9a40c438af7c0ed005e2e7
datnt700 <81250707+datnt700@users.noreply.github.com>
2021-03-23 23:18:56 +0700

    Add files via upload

---
+++ nashtech/NashTech/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        public class PrintNumber
+        {
+            public static void PrimeNumber()
+            {
+                for (var i = 2; i < 100; i++)
+                {
+                    bool isPrimeNumber = true;
+                    for (var j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrimeNumber = false;
+                            break;
+                        }
+                    }
+                    if (isPrimeNumber)
+                    {
+                        Console.Write(i + "-");
+                    }
+                }
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            PrintNumber.PrimeNumber();
+            Console.ReadLine();
+        }
+    }
+}
